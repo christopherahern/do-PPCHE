6b074f4c59004cd1d9d3d3d7a437ffb425f81ecb
bacovcin <bacovcin@babel.ling.upenn.edu>
2016-05-06 17:12:12 -0400

    Implemented new system for adding metadata to corpus search output

---
+++ add_metadata.py
@@ -0,0 +1,71 @@
+# Function that returns an integer if the string is an integer and
+# -1000 otherwise
+
+
+def tryint(x):
+    try:
+        return(int(x))
+    except:
+        return(-1000)
+
+
+# Generates a python database and adds correct metadata to file
+
+dbfile = open('./database/English_database.txt').readlines()
+
+names = dbfile[0].rstrip()
+
+db = {}
+# Database is a dictionary of dicitonaries
+# Key of first dictionary: file id
+# Key of second dictionary: tuple of start token num and end token num
+# For single text files the token ids = x
+for line in dbfile[1:]:
+    s = line.rstrip().split(':')
+    print(s[0])
+    try:
+        db[s[0]][(s[1], s[2])] = line.rstrip()
+    except:
+        db[s[0]] = {}
+        db[s[0]][(s[1], s[2])] = line.rstrip()
+
+# Add the non-metadata column labels
+names += open('./database/CodingNames.txt').readline()
+
+# Open the final file
+outfile = open('do-support.txt', 'w')
+outfile.write(names)
+
+# Load the coding output
+coded_file = open('./queries/do-support.cod.ooo').readlines()
+
+# Walk through output and append appropriate metadata
+keyerrors = []
+i = 0
+filelen = len(coded_file)
+for line in coded_file:
+    i += 1
+    print (str(i) + ' out of ' + str(filelen))
+    codes = line.split('@')[0] + '\n'
+    token = line.split('@')[1].split('.')[-1]
+    filename = line.split('@')[1].split(',')[0].lower()
+    try:
+        for text in db[filename].keys():
+            # Check if file is:
+            # Single text (i.e. startid == 'x'
+            # or if the tokenid is between the text's start and end ids
+            if ((text[0] == 'x') or
+               ((tryint(token) >= tryint(text[0]) and
+                 (tryint(token) <= tryint(text[1]))))):
+                outfile.write(db[filename][text] +
+                              '\t' +
+                              token +
+                              '\t' +
+                              codes)
+    except:
+        if filename not in keyerrors:
+            keyerrors.append(filename)
+
+outfile = open('missing_keys.txt', 'w')
+for filename in keyerrors:
+    outfile.write(filename + '\n')
--- queries/do-support.c
+++ queries/do-support.c
@@ -1,13 +1,13 @@
 node: CP*
 coding_query:
 
-2: {
+1: {
 MOD: (CP* iDoms IP*) AND (IP* iDomsMod IP*|CONJ* BAG*|BE*|BED*|BEI*|BEN*|BEP*|HAG*|HAN*|HV*|HVD*|HVI*|HVN*|HVP*|MD*|MD0*)
 DO: (CP* iDoms IP*) AND (IP* iDomsMod IP*|CONJ* DAG*|DAN*|DO*|DOD*|DOI*|DON*|DOP*)
 NOTDO: ELSE
 }
 
-3: {
+2: {
 SUBQ: (CP* hasLabel CP-QUE-SUB*)
 SBJQ: (CP* hasLabel CP-QUE*) AND (CP* idoms WNP*) AND (CP* iDomsMod IP*|CONJ*|NP-SBJ* \*T\**) AND (WNP* sameIndex \*T\**)
 QUE: (CP* hasLabel CP-QUE*)
@@ -16,7 +16,7 @@ DEC: (CP* iDomsMod IP*|CONJ* IP-MAT*|IP-SUB*)
 OTHER: ELSE
 }
 
-4: {
+3: {
 NEG: (CP* iDoms IP*) AND (IP* iDomsMod IP*|CONJ* NEG*)
 AFF: (CP* iDoms IP*)
 }
